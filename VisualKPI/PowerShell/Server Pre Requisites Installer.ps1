efcbd73368f54b7519044d1f066bf26285d83b43
ArtruneTranspara <48358611+ArtruneTranspara@users.noreply.github.com>
2019-03-08 17:04:09 -0600

    Add files via upload

---
+++ VisualKPI/PowerShell/Server Pre Requisites Installer.ps1
@@ -0,0 +1,20 @@
+﻿""
+"Installing web server - IIS "
+Install-WindowsFeature -Name Web-Server -IncludeManagementTools
+""
+"Instaling Basic Authentication"
+#Install-WindowsFeature -Name Web-Dyn-Compression
+Install-WindowsFeature -Name Web-Basic-Auth
+""
+"Installing Windows Authentication"
+Install-WindowsFeature -Name Web-Windows-Auth
+""
+"Installing .NET extensibility"
+Install-WindowsFeature -Name Web-Net-Ext45 
+""
+"Installing ASP"
+Install-WindowsFeature -Name Web-Asp-Net45 
+#Install-WindowsFeature -Name Web-Mgmt-Service 
+
+pause
+

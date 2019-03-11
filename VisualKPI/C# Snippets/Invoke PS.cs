84f33b903cf50ca5369a4ad2cbc3c36834d6c73d
ArtruneTranspara <48358611+ArtruneTranspara@users.noreply.github.com>
2019-03-11 10:20:06 -0600

    Update Invoke PS.cs

--- VisualKPI/C# Snippets/Invoke PS.cs
+++ VisualKPI/C# Snippets/Invoke PS.cs
@@ -1,1 +1,13 @@
-
+            var pss = @"C:\Users\IGNITE\Desktop\csharp.ps1";//root to the ps
+            Process cmd = new Process();//create a CMD to run powershell, directly running powershell didn't seem to work
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+            cmd.StandardInput.WriteLine("Powershell Set-ExecutionPolicy -Scope CurrentUser -ExecutionPolicy Bypass -Force;");//This enables execution of powershell scripts 
+            cmd.StandardInput.WriteLine("PowerShell.exe -windowstyle hidden " + pss + " ");//Runs a powershell script without window
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();

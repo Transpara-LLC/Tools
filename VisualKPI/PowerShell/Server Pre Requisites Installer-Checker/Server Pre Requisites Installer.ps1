e74d05abafff1d2fc275279e5257d495d1fffe84
ArtruneTranspara <48358611+ArtruneTranspara@users.noreply.github.com>
2019-03-22 12:13:37 -0600

    Add files via upload

---
+++ VisualKPI/PowerShell/Server Pre Requisites Installer-Checker/Server Pre Requisites Installer.ps1
@@ -0,0 +1,64 @@
+﻿[system.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
+function installPreRequisites {
+    ""
+    "Installing web server - IIS "
+    Install-WindowsFeature -Name Web-Server -IncludeManagementTools
+    ""
+    "Instaling Basic Authentication"
+    #Install-WindowsFeature -Name Web-Dyn-Compression
+    Install-WindowsFeature -Name Web-Basic-Auth
+    ""
+    "Installing Windows Authentication"
+    Install-WindowsFeature -Name Web-Windows-Auth
+    ""
+    "Installing .NET extensibility"
+    Install-WindowsFeature -Name Web-Net-Ext45 
+    ""
+    "Installing ASP"
+    Install-WindowsFeature -Name Web-Asp-Net45 
+    #Install-WindowsFeature -Name Web-Mgmt-Service 
+}
+function checkPreRequisites {
+    $feature =Get-WindowsFeature *Web-Server*
+    $installed = $feature.Installed
+    echo "Web-Server (IIS) installed:      $installed"
+
+    $feature =Get-WindowsFeature *Web-Basic-Auth*
+    $installed = $feature.Installed
+    echo "Basic Authentication installed:  $installed"
+
+    $feature =Get-WindowsFeature *Web-Windows-Auth*
+    $installed = $feature.Installed
+    echo "Windows Authentication:          $installed"
+
+    $feature =Get-WindowsFeature *Web-Net-Ext45*
+    $installed = $feature.Installed
+    echo ".NET extensibility installed:    $installed"
+
+    $feature =Get-WindowsFeature *Web-Asp-Net45*
+    $installed = $feature.Installed
+    echo "ASP installed:                   $installed"
+}
+
+        
+
+
+try {
+$action = [Microsoft.VisualBasic.Interaction]::InputBox("Enter install for installing or check for checking the installations", "Action Request")
+if($action -eq "check") {
+    ""
+    "-------CHECKING PREREQUISITES---------"
+    checkPreRequisites
+    "-------END CHECKING PREREQUISITES---------"
+    ""
+
+}
+if($action -eq "install") {
+    ""
+    "-------INSTALLING PREREQUISITES-------"
+    installPreRequisites
+    "-------END INSTALLING PREREQUISITES-------"
+    ""
+}
+pause
+}catch {}
---
+++ VisualKPI/PowerShell/Server Pre Requisites Installer-Checker/readme.txt
@@ -0,0 +1,2 @@
+Instructions
+Type install to install the features or type check to see which features are installed

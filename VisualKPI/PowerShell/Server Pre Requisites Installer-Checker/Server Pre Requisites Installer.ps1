7c742d31133051b6d1719e3a0bfa405449f6f900
ArtruneTranspara <48358611+ArtruneTranspara@users.noreply.github.com>
2019-04-15 17:13:12 -0500

    Added dot net 4.7.2

--- VisualKPI/PowerShell/Server Pre Requisites Installer-Checker/Server Pre Requisites Installer.ps1
+++ VisualKPI/PowerShell/Server Pre Requisites Installer-Checker/Server Pre Requisites Installer.ps1
@@ -17,27 +17,46 @@ function installPreRequisites {
     "Installing ASP"
     Install-WindowsFeature -Name Web-Asp-Net45 
     #Install-WindowsFeature -Name Web-Mgmt-Service 
+    ""
+
+    "Installing .NET 4.7.2"
+    installDotNet472
+}
+function installDotNet472{
+$source = "https://download.microsoft.com/download/6/E/4/6E48E8AB-DC00-419E-9704-06DD46E5F81D/NDP472-KB4054530-x86-x64-AllOS-ENU.exe";
+$destination = "$PSScriptRoot\dotnet47.exe"
+#Check if the installer is in the folder. If installer exist, replace it
+If ((Test-Path $destination) -eq $false) {
+    New-Item -ItemType File -Path $destination -Force
+} 
+#install software
+Invoke-WebRequest $source -OutFile $destination
+Start-Process -Wait -FilePath "$destination" -ArgumentList "/S" -PassThru
 }
 function checkPreRequisites {
     $feature =Get-WindowsFeature *Web-Server*
     $installed = $feature.Installed
-    echo "Web-Server (IIS) installed:      $installed"
+    "Web-Server (IIS) installed:      $installed"
 
     $feature =Get-WindowsFeature *Web-Basic-Auth*
     $installed = $feature.Installed
-    echo "Basic Authentication installed:  $installed"
+    "Basic Authentication installed:  $installed"
 
     $feature =Get-WindowsFeature *Web-Windows-Auth*
     $installed = $feature.Installed
-    echo "Windows Authentication:          $installed"
+    "Windows Authentication:          $installed"
 
     $feature =Get-WindowsFeature *Web-Net-Ext45*
     $installed = $feature.Installed
-    echo ".NET extensibility installed:    $installed"
+    ".NET extensibility installed:    $installed"
 
     $feature =Get-WindowsFeature *Web-Asp-Net45*
     $installed = $feature.Installed
-    echo "ASP installed:                   $installed"
+    "ASP installed:                   $installed"
+
+
+    $installed = (Get-ItemProperty "HKLM:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full").Release -ge 461814
+    ".NET 4.7.2 installed:            $installed"
 }
 
         
